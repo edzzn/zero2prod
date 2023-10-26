b927a97da3ad2f6b32001c73893f49b4803098c8
Edisson Reinozo <edisson350@gmail.com>
2023-10-25 19:41:48 -0500

    refactor app, reorganize

---
+++ src/startup.rs
@@ -0,0 +1,17 @@
+use actix_web::{dev::Server, HttpServer, App, web};
+use crate::routes::{health_check, subscribe};
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(||
+         App::new().
+         route(
+            "/health_check", web::get().to(health_check)
+        )
+        .route("/subscribe", web::post().to(subscribe))
+    )
+        .listen(listener)?
+        .run();
+
+    Ok(server)
+}
