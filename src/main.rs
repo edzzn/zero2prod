7a9bf32f1b7e94c95cbae74617f52076f28997e1
Edisson Reinozo <edisson350@gmail.com>
2023-10-28 21:46:15 -0500

    improve tracing

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
+mod telemetry;
+
 use sqlx::postgres::PgPool;
-use tracing_log::LogTracer;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // Required by Sentry
     std::env::set_var("RUST_BACKTRACE", "full");
-    // Redirect all `log`'s events to our subscriber
-    LogTracer::init().expect("Failed to set logger");
 
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber(
+        String::from("zero2prod"),
+        String::from("info"),
+        std::io::stdout,
+    );
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
