0ecd110d196d9a24a7d96fa33edf1cb0d86c6c9c
Edisson Reinozo <edisson350@gmail.com>
2023-10-28 22:14:34 -0500

    fix tracing

--- src/startup.rs
+++ src/startup.rs
@@ -1,23 +1,26 @@
 use crate::configuration::get_configuration;
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let _guard = sentry::init((configuration.sentry_url, sentry::ClientOptions {
-        release: sentry::release_name!(),
-        ..Default::default()
-      }));
+    let _guard = sentry::init((
+        configuration.sentry_url,
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            ..Default::default()
+        },
+    ));
 
     let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .wrap(sentry_actix::Sentry::new())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
